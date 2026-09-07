8f8d843a1f6c6f87443135953cfaf6c1c48cd8e1
paulmullane <paul24mullane@gmail.com>
2026-09-07 14:23:07 +0100

    Update Glass_Packaging_Waste.R

--- Selecting optimal models/Glass_Packaging_Waste.R
+++ Selecting optimal models/Glass_Packaging_Waste.R
@@ -290,7 +290,7 @@ rolling_cv_cubist <- function(train_data, covariates){
 }
 
 #read waste data-----
-Glass_Packaging_Waste <- read_excel("C:/Users/20229798/OneDrive - University of Limerick/Desktop/Packaging waste forecasting/Packaging Data/glass_packaging_waste.xlsx")
+Glass_Packaging_Waste <- read_excel("Desktop/Packaging waste forecasting/Packaging Data/glass_packaging_waste.xlsx")
 Glass_Packaging_Waste$TIME <- as.double(Glass_Packaging_Waste$TIME)
 
 #read country data----
@@ -301,7 +301,7 @@ countries <- c("Austria", "Belgium", "Denmark", "Finland", "France", "Germany",
 vars <- c("Year", "Population", "GDP", "material_footprint", "energy_consumption", "co2", "exports")
 
 modelling_data <- lapply(setNames(countries, tolower(countries)), function(ctry) {
-  df <- read_excel(paste0("C:/Users/20229798/OneDrive - University of Limerick/Desktop/Packaging waste forecasting/Country data/", ctry, ".xlsx")) |>
+  df <- read_excel(paste0("Desktop/Packaging waste forecasting/Country data/", ctry, ".xlsx")) |>
     dplyr::select(all_of(vars)) |>
     filter(Year >= 1997, Year <= 2022)
   df$gpw <- Glass_Packaging_Waste[[ctry]][Glass_Packaging_Waste$TIME <= 2022]
